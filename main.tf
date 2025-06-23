76f8d3dc8d308cdb33ba0b480a247a204460cb11
Michal <michal.potoczny92@gmail.com>
2025-06-23 15:25:07 +0200

    feat: Add IAM Role and Index for Amazon Kendra but not apply to AWS yet

--- main.tf
+++ main.tf
@@ -27,3 +27,44 @@ resource "aws_s3_bucket" "rag_documents_poc_bucket" {
     Owner       = "michal.potoczny"
   }
 }
+
+# --- Amazon Kendra Resources ---
+
+# IAM Role to grant Kendra permissions to operate on our behalf
+resource "aws_iam_role" "kendra_role" {
+  name = "AegisRAG-Kendra-Role-PoC"
+
+  # Trust policy allowing the Kendra service to assume this role
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "kendra.amazonaws.com"
+      }
+    }]
+  })
+}
+
+# The main Kendra Index resource
+resource "aws_kendra_index" "aegis_rag_index" {
+  # Name of the index as it will appear in the AWS Console
+  name = "aegis-rag-poc-index"
+  
+  # IMPORTANT: Using the Developer Edition to be eligible for the Free Tier
+  edition = "DEVELOPER_EDITION"
+
+  # Assign the IAM role created above to this index.
+  # Terraform understands this dependency and will create the role first.
+  role_arn = aws_iam_role.kendra_role.arn
+
+  description = "Index for the Aegis RAG PoC project."
+
+  tags = {
+    Name        = "Aegis RAG PoC - Kendra Index"
+    Environment = "PoC"
+    Project     = "Aegis-RAG"
+    Owner       = "michal.potoczny" # Remember to change this to your details
+  }
+}
